07839626cf677fe764faa94c1c23d353ea8e9e41
taochanglian <taochanglian@163.com>
2017-01-20 13:33:42 +0800

    no message

---
+++ config-server/src/main/java/com/mycompany/microserver/configserver/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.mycompany.microserver.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by tao on 17/1/19.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class,args);
+    }
+}
---
+++ eureka-server/src/main/java/com/mycompany/microservice/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.mycompany.microservice.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by tao on 17/1/18.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
